0d19e47bd2f5eaf4752a905d48e81ca6fa1046f2
bbasanta238 <bbasanta238@gmail.com>
2022-08-05 15:12:10 +0545

    connectingMetamask-with-web3

--- counterfeit/src/App.js
+++ counterfeit/src/App.js
@@ -1,46 +1,27 @@
 // import logo from './logo.svg';
 import "./App.css";
-import React, { useEffect } from "react";
+// import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarComp from "./components/NavbarComp";
 import AboutComp from "./components/AboutComp";
-import init from "./hooks/Web3Client";
+// import UseWeb3 from "./hooks/Web3Client";
 function App() {
-	// connecting with blockchain Network
-	// const providerUrl =  "HTTP://127.0.0.1:7545";
-
-	//use of useEffect hook to use the function when ever it renders if the cirteria meets
-	useEffect(() => {
-		//connecting with metamask wallet
-		// let provider = window.ethereum;
-
-		// if (typeof provider !== "undefined") {
-		// 	//if provider is not undefined then metamask is installed by user
-		// 	//requestign to connect metamask with blockchain network
-		// 	provider
-		// 		.request({ method: "eth_requestAccounts" })
-		// 		.then((accounts) => {
-		// 			//since provider.request is a promise so we have to wiat for resolve
-		// 			console.log(accounts);
-		// 			// <h1>{accounts}</h1>
-		// 		})
-		// 		.catch((err) => {
-		// 			console.log(err);
-		// 		});
-		// 	window.ethereum.on("accountsChanged", function (acccounts) {
-		// 		console.log(acccounts);
-		// 	});
-		// }
-		// const web3 = new Web3(provider);
-	}, []);
+	const { render,account } = NavbarComp();
 	return (
 		<>
 			<Router>
-				<NavbarComp />
+				{render}
 				<Routes>
 					{/* <Route path="/" element={<App/>}/> */}
 					<Route path="/about" element={<AboutComp />} />
 				</Routes>
+
+				<h1>
+					hi{account}
+					{/* {console.log(connect)}
+					{console.log(errorMessage)} */}
+					{/* Accounts : {provider} ----{account} */}
+				</h1>
 			</Router>
 		</>
 	);
--- counterfeit/src/hooks/Web3Client.js
+++ counterfeit/src/hooks/Web3Client.js
@@ -1,26 +1,72 @@
-import { useState } from "react";
-import Web3 from 'web3';
+import Web3 from "web3";
+import { useState, useEffect } from "react";
 
-const useWeb3 = (setIsLoading, setErrorMessage)=>{
+const useWeb3 = (setIsLoading, setErrorMessage) => {
 	//web3 provider instance
-	const [provider, setProvider] = useState(null);//initially web3 provider is null as no account is connected
+	const [provider, setProvider] = useState(null); //initially web3 provider is null as no account is connected
 
-	//settign active account
-	const [account, setAccount] = useState('');
+	//address of active account
+	const [account, setAccount] = useState(null);
+
+	//handle on every account changed
+	useEffect(() => {
+		
+		//if account is lock directly from metamask then account is set as undefined which is chaged to null
+		if (account === undefined) {
+			setAccount(null);
+		}
+	}, [account]);
 
 	//connecting this function to a button component
-
-	const connect = async () =>{
-		try{
+	const connect = async () => {
+		try {
 			setIsLoading(true);
-			setErrorMessage('');
+			setErrorMessage("");
+			let metamaskInject = window.ethereum;
 			//checking Metamask is installed or not and throw error if not found that is catched and error is shown using prompt
-			if(!window.ethereum) throw new Error('Please Install Metamask');
+			if (typeof metamaskInject == "undefined") {
+				throw new Error("Please Install Metamask");
+			}
+			//connecting metamask
+			await window.ethereum.request({
+				method: "eth_requestAccounts",
+			});
 
 			//connect metamask to web3.js and get a web3 provider instance
-			const web3 = new We;
-		}catch{
+			const web3 = new Web3(metamaskInject); //instant of web3
+			const accounts = await web3.eth.getAccounts();
+			setAccount(accounts[0]);
+			setProvider(web3);
+			console.log("from web3" + accounts[0]);
+			console.log("from web3 account" + account);
+
+			//handling on account changed
+			window.ethereum.on("accountsChanged", (accounts) => {
+				setAccount(accounts[0]);
+			});
+		} catch (error) {
+			//error code -3002 is Already processing eth_requestAccounts. Please wait. when the metamask connection async function gets terminated withour resolve
+			if (error.code === -32002) {
+				setErrorMessage("-32002");
+				console.log("please retry");
+			}
+			window.alert(error.message);
+			// console.log(error.message);
+			setErrorMessage(error.message);
+		} finally {
 			
+			setIsLoading(false);
 		}
-	}
-}
+	};
+
+	//connecting this button to disconnect to metamask
+	const disconnect = () => {
+		console.log("Disconnecting");
+		setProvider(null);
+		setAccount(null);
+		// console.log(account);
+	};
+	return { connect, disconnect, provider, account };
+};
+
+export default useWeb3;
